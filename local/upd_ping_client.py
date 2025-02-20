ddd1b034a31ca529738adf44e91b72702149432d
Deepanshu Yadav <Deeepanshu3107@gmail.com>
2025-02-20 23:31:45 +0530

    udp-ping-client-updated

--- local/upd_ping_client.py
+++ local/upd_ping_client.py
@@ -1,22 +1,17 @@
 import socket
 import time
 
-# Server details
 SERVER_IP = "35.170.185.50"
 SERVER_PORT = 9999
 
-# Create UDP socket
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 try:
     while True:
         message = "ping"
         client.sendto(message.encode(), (SERVER_IP, SERVER_PORT))  # Send ping request
-
-        # Receive response
-        data, _ = client.recvfrom(1024)
-        print(f"Server Response: {data.decode()}")
-
+        data, _ = client.recvfrom(1024)  # Receive response
+        print(f"Server Response: {data.decode()}")  # Print response
         time.sleep(1)  # Wait 1 second before sending the next ping
 
 except KeyboardInterrupt:
